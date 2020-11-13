f084b36fea23e1f9bdbeed838e1ab07394d345e1
Dave Carlton <davec@polymicro.net>
2020-11-12 18:37:11 -0600

    Updated

--- Sources/PMNLibrary/PMNLibrary.swift
+++ Sources/PMNLibrary/PMNLibrary.swift
@@ -1,3 +1,65 @@
+
+import SwiftUI
+
 struct PMNLibrary {
-    var text = "Hello, World!"
+    var text = "polymicro.net library"
 }
+
+@discardableResult
+func share(
+    items: [Any],
+    excludedActivityTypes: [UIActivity.ActivityType]? = nil
+) -> Bool {
+    guard let source = UIApplication.shared.windows.last?.rootViewController else {
+        return false
+    }
+    let vc = UIActivityViewController(
+        activityItems: items,
+        applicationActivities: nil
+    )
+    vc.excludedActivityTypes = excludedActivityTypes
+    vc.popoverPresentationController?.sourceView = source.view
+    source.present(vc, animated: true)
+    return true
+}
+
+class ActivityViewController : UIViewController {
+    
+    var uiImage:UIImage!
+    var ideaTitle: String!
+    var ideaDescription: String!
+    
+    @objc func shareImage() {
+        let vc = UIActivityViewController(activityItems: [uiImage!], applicationActivities: [])
+        vc.excludedActivityTypes =  [
+            UIActivity.ActivityType.postToWeibo,
+            UIActivity.ActivityType.assignToContact,
+            UIActivity.ActivityType.addToReadingList,
+            UIActivity.ActivityType.postToVimeo,
+            UIActivity.ActivityType.postToTencentWeibo
+        ]
+        present(vc,
+                animated: true,
+                completion: nil)
+        vc.popoverPresentationController?.sourceView = self.view
+    }
+    
+}
+
+struct SwiftUIActivityViewController : UIViewControllerRepresentable {
+    
+    let activityViewController = ActivityViewController()
+    
+    func makeUIViewController(context: Context) -> ActivityViewController {
+        activityViewController
+    }
+    func updateUIViewController(_ uiViewController: ActivityViewController, context: Context) {
+        //
+    }
+    func shareImage(uiImage: UIImage) {
+        activityViewController.uiImage = uiImage
+        activityViewController.shareImage()
+    }
+}
+
+
