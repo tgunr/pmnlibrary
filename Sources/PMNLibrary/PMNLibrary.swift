79dd759ffdb2fd8adf1242a9230f3a3bb76934f1
Dave Carlton <davec@polymicro.net>
2020-11-16 09:52:03 -0600

    Removed share

--- Sources/PMNLibrary/PMNLibrary.swift
+++ Sources/PMNLibrary/PMNLibrary.swift
@@ -5,24 +5,6 @@ struct PMNLibrary {
     var text = "polymicro.net library"
 }
 
-@discardableResult
-func share(
-    items: [Any],
-    excludedActivityTypes: [UIActivity.ActivityType]? = nil
-) -> Bool {
-    guard let source = UIApplication.shared.windows.last?.rootViewController else {
-        return false
-    }
-    let vc = UIActivityViewController(
-        activityItems: items,
-        applicationActivities: nil
-    )
-    vc.excludedActivityTypes = excludedActivityTypes
-    vc.popoverPresentationController?.sourceView = source.view
-    source.present(vc, animated: true)
-    return true
-}
-
 class ActivityViewController : UIViewController {
     
     var uiImage:UIImage!
